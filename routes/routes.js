92e01fa3b0d0a373da104fbe44de745951592511
aiyus <aiyusboss@gmail.com>
2023-07-05 18:07:33 +0100

    more work

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,62 @@
 const express = require("express");
 const router = express.Router();
-const client = require("../db");
-router.post("/user/api/v1", async (req, res) => {
+const client = require("../models/db");
+const bycrypt = require("bcrypt");
+const jwtGenerator = require("../utlis/jwtGenerator");
+
+router.post("/api/v1/register", async (req, res) => {
   try {
-    const { email, password } = req.body;
-    const register = await client.query(
-      "INSERT INTO register (email, password) VALUES ($1, $2) RETURNING *",
-      [email, password]
+    const {
+      id,
+      email,
+      password,
+      firstname,
+      lastname,
+      gender,
+      jobrole,
+      department,
+      address,
+    } = req.body;
+    //check/validate if user exist in the databasa
+    const user = await client.query("SELECT * FROM register WHERE email = $1", [
+      email,
+    ]);
+
+    if (user.rows.length !== 0) {
+      return res.status(401).send("user already exist");
+    }
+    //encrypt/hash password
+
+    const saltRound = 10;
+    const salt = await bycrypt.genSalt(saltRound);
+
+    const hashedPassword = await bycrypt.hash(password, salt);
+
+    // creating new user
+    const newUser = await client.query(
+      "INSERT INTO register (email, password, firstname, lastname, gender, jobrole, department,address) VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",
+      [
+        email,
+        hashedPassword,
+        firstname,
+        lastname,
+        gender,
+        jobrole,
+        department,
+        address,
+      ]
     );
-    res.json("done");
+    const token = jwtGenerator(newUser.rows[0].id);
+    res.json({
+      status: "success",
+      data: {
+        message: "user account successfully created",
+        token,
+        userId: id,
+      },
+    });
   } catch (error) {
-    console.error(error);
+    console.error({ status: "error", errro: error });
   }
 });
 
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const client = require("./db");
 const users = require("./routes/routes");
 
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use("/routes", users);
+app.use("/", users);
 
 const PORT = process.env.PORT || 5000;
 //listening for server
