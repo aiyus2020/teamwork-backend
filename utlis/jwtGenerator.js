24fe915b90582d280f3604a35d8abbe0d8f36168
aiyus <aiyusboss@gmail.com>
2023-08-03 10:39:43 +0100

    remove gifstest

--- utlis/jwtGenerator.js
+++ utlis/jwtGenerator.js
@@ -5,6 +5,6 @@ function jwtGenerator(id) {
   const payload = {
     user: id,
   };
-  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "5hr" });
+  return jwt.sign(payload, process.env.jwtSecret, { expiresIn: "24hr" });
 }
 module.exports = jwtGenerator;
