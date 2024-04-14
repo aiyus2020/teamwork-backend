dc5656033b09818425534ccdcae604fae97bb693
aiyus <aiyusboss@gmail.com>
2024-04-14 18:40:25 +0100

    supper base string

--- models/db.js
+++ models/db.js
@@ -6,7 +6,7 @@ const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsYXdpeXVtanB5eW90aXpkYnppIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5NDk0MTU3OCwiZXhwIjoyMDEwNTE3NTc4fQ.9gqzn3MErIurwTj_KcjnZT4C8D8s-Gurn7Rt5WPqHB0";
 
 const client = new Client({
-  connectionString: `postgresql://postgres:aiyudubie10@db.ulawiyumjpyyotizdbzi.supabase.co:5432/postgres`,
+  connectionString: `postgres://postgres.ulawiyumjpyyotizdbzi:aiyudubie10@aws-0-ca-central-1.pooler.supabase.com:5432/postgres`,
 });
 
 client.connect(function (err) {
